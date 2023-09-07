8ad53dea6678ddf8ef8643ff40b7766cf2e310d4
Fatma Ashraf <129579797+FattemaAshraf@users.noreply.github.com>
2023-09-07 20:26:13 +0300

    created Games Create Action

--- GameZone/Controllers/GamesController.cs
+++ GameZone/Controllers/GamesController.cs
@@ -1,9 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameZone.Data;
+using GameZone.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameZone.Controllers
 {
     public class GamesController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
             return View();
@@ -11,7 +18,35 @@ namespace GameZone.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            return View();
+            CreateGameFormViewModel viewModel = new()
+            {
+                //initailizing data from context to IEnumable<> Categories
+                Categories = _context.Categories
+                                     .Select(c => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Value = c.Id.ToString(), Text = c.Name })
+                                     .OrderBy(c => c.Text)
+                                     .ToList(),
+                Devices = _context.Devices
+                                     .Select(d => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem { Value = d.Id.ToString(), Text = d.Name })
+                                     .OrderBy(d => d.Text)
+                                     .ToList()
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken] // for input _RequestVerficationToken to security form before submitted in database from attackers
+
+        public IActionResult Create(CreateGameFormViewModel model)
+        {
+            //server side validation
+            if (!ModelState.IsValid) 
+            {
+                return View();
+            }
+
+            //save game to database
+            //save cover to server
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
